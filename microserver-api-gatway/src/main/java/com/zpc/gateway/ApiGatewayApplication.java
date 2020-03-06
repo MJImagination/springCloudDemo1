896be30df7f23ea1cc9a450fd428f270a4039e44
MJ <798282260@qq.com>
2020-03-06 15:51:33 +0800

    zuulfilter

--- microserver-api-gatway/src/main/java/com/zpc/gateway/ApiGatewayApplication.java
+++ microserver-api-gatway/src/main/java/com/zpc/gateway/ApiGatewayApplication.java
@@ -3,12 +3,12 @@ package com.zpc.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableZuulProxy
+@ComponentScan(basePackages = "com.zpc.gateway.filter")
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
--- readme.md
+++ readme.md
@@ -9,5 +9,10 @@ http://127.0.0.1:8081/item/7
 http://127.0.0.1:8082/order/201810300001
 http://127.0.0.1:8100/
 http://127.0.0.1:8082/consumer
+
 http://127.0.0.1:8087/item-service/item/7
+http://127.0.0.1:8087/order-service/order/201810300001
+
+zuulfilter :http://127.0.0.1:8087/order-service/order/201810300001?token=1
 ````
+
